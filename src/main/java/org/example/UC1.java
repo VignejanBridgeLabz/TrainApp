3bf4be745efce70f4e26d97e81f91ec9bb22d385
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 12:22:28 +0530

    commit the UC1

---
+++ src/main/java/org/example/UC1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.*;
+import java.util.List;
+
+public class UC1 {
+
+    public static void main(String[] args) {
+    List<String> bogies=new ArrayList<String>();
+        System.out.println("Train Consist Management App");
+        System.out.println("Initial Bogie Count: "+bogies.size());
+
+    }
+
+}
